1b17a4714bef00e5f7c5fabde720eca6ca79570f
grezar <grezar.dev@gmail.com>
2019-07-14 09:21:47 +0700

    Validate the number of arguments

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+    "fmt"
     "os"
 )
 
 func main() {
+    if len(os.Args) != 2 {
+        fmt.Printf("Invalid number of arguments (expected 2, got %d)\n", len(os.Args))
+        os.Exit(1)
+    }
     os.Exit(run())
 }
 
